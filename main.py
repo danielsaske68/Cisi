f53e3f5366e02aada658e88268819d9872a66260
juan daniel giraldo alzate <danielsaske68@hotmail.com>
2026-08-20 11:28:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -296,4 +296,5 @@ def descargar_archivo(nombre):
     return send_from_directory(DATA_DIR, secure_filename(nombre), as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
